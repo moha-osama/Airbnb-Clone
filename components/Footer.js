de8ce6bcf56faadfe5f81dda7130292c0a2dd46e
mohamed <zmuhamedosama@gmail.com>
2023-05-23 16:15:29 +0300

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <footer className=" bg-gray-100 text-gray-600">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 max-w-[80rem] m-auto">
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">Support</h5>
+          <p>Help Centre</p>
+          <p>AirCover</p>
+          <p>Supporting people with disabilities</p>
+          <p>Cancellation options</p>
+          <p>Our COVID-19 Response</p>
+          <p>Report a neighbourhood concern</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">Community</h5>
+          <p>Airbnb.org: disaster relief housing</p>
+          <p>Combating discrimination</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">Hosting</h5>
+          <p>Airbnb your home</p>
+          <p>AirCover for Hosts</p>
+          <p>Explore hosting resources</p>
+          <p>Visit our community forum</p>
+          <p>How to host responsibly</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">Airbnb</h5>
+          <p>Newsroom</p>
+          <p>Learn about new features</p>
+          <p>Letter from our founders</p>
+          <p>Careers</p>
+          <p>Investors</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
